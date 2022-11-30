d01b0d9bd3922ee640aab63ce2171c85598b461b
xiaoxigua-1 <aijdfajodwsdf@gmail.com>
2022-11-30 21:07:04 +0800

    fix format

--- crates/x-engine/src/repl.rs
+++ crates/x-engine/src/repl.rs
@@ -1,6 +1,9 @@
 use x_input::Input;
 use x_parser::{Lexer, Parser};
-use x_protocol::{crossterm::style::{Stylize, StyledContent}, ShellErr};
+use x_protocol::{
+    crossterm::style::{StyledContent, Stylize},
+    ShellErr,
+};
 
 pub fn repl(input: &mut Input) -> String {
     let raw_input = input.user_input.clone();
@@ -28,27 +31,39 @@ pub fn repl(input: &mut Input) -> String {
         }
     }
 
-    format!("{} ", output.iter().map(|s| { s.to_string() }).collect::<Vec<_>>().join(""))
+    format!(
+        "{} ",
+        output
+            .iter()
+            .map(|s| { s.to_string() })
+            .collect::<Vec<_>>()
+            .join("")
+    )
 }
 
-fn error_header(e: ShellErr, raw_input: &String, output: &mut Vec<StyledContent<String>>, parser: &mut Parser) -> Option<StyledContent<String>> {
+fn error_header(
+    e: ShellErr,
+    raw_input: &String,
+    output: &mut Vec<StyledContent<String>>,
+    parser: &mut Parser,
+) -> Option<StyledContent<String>> {
     match e {
         x_protocol::ShellErr::Syntax(range, _) => Some(raw_input[range].to_string().red()),
-        x_protocol::ShellErr::UnterminatedStr(range) => {
-            Some(format!(
+        x_protocol::ShellErr::UnterminatedStr(range) => Some(
+            format!(
                 "{}{}",
                 raw_input[range.clone()].red(),
                 raw_input[range.end..].green()
-            ).stylize())
-        }
+            )
+            .stylize(),
+        ),
         x_protocol::ShellErr::Unterminated(_, i, _) => {
             output[i] = output[i].clone().red();
             if let Err(e) = parser.eat_remaining_tokens() {
                 error_header(e, raw_input, output, parser);
             }
-            None 
+            None
         }
         x_protocol::ShellErr::EOF => None,
     }
 }
-
