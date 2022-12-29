6f80b095754db1e7d39c8b36d4e0cbf8c4742c2a
xiaoxigua-1 <aijdfajodwsdf@gmail.com>
2022-12-29 13:56:23 +0800

    fix format

--- crates/x-checker/src/lib.rs
+++ crates/x-checker/src/lib.rs
@@ -1,11 +1,12 @@
-use x_protocol::ast::AST;
+use x_protocol::{ast::AST, ShellState};
 
 pub struct Checker {
     asts: Vec<AST>,
+    state: ShellState,
 }
 
 impl Checker {
-    pub fn new(asts: Vec<AST>) -> Self {
-        Checker { asts }
+    pub fn new(asts: Vec<AST>, state: ShellState) -> Self {
+        Checker { asts, state }
     }
 }
