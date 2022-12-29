e2ab498a8a9fd6c6bb7535caf611841319474436
xiaoxigua-1 <aijdfajodwsdf@gmail.com>
2022-12-29 13:31:44 +0800

    fix format

--- src/cli.rs
+++ src/cli.rs
@@ -1,26 +1,25 @@
-use std::{error::Error, path::PathBuf};
 use clap::Parser;
+use std::{error::Error, path::PathBuf};
 
 #[derive(Parser, Debug)]
 #[command(name = "xshell")]
 #[command(author, version, about, long_about = None)]
 pub struct Args {
-    
     /// set environment variable
     #[arg(short = 'e', long = "env", value_parser = parse_key_val::<String, String>)]
     envs: Vec<(String, String)>,
-   
+
     /// set config file path
     #[arg(long = "config")]
     config: Option<PathBuf>,
 
-    /// run script 
+    /// run script
     #[arg(last = true)]
     path: Vec<PathBuf>,
-   
+
     /// run the given commands and then exit
     #[arg(short = 'c', long)]
-    command: Option<String>
+    command: Option<String>,
 }
 
 /// Parse a single key-value pair
@@ -36,4 +35,3 @@ where
         .ok_or_else(|| format!("invalid KEY=value: no `=` found in `{}`", s))?;
     Ok((s[..pos].parse()?, s[pos + 1..].parse()?))
 }
-
